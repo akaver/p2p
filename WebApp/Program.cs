447214319db3cac0c366951fe8cf84426698c682
Andres Käver <akaver@akaver.com>
2019-02-07 09:42:12 +0200

    Make server port configurable from command line

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -12,13 +12,23 @@ namespace WebApp
 {
     public class Program
     {
+        private static int _portToListen = 5000;
         public static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (int.TryParse(args[0], out var portFromArgs))
+                {
+                    _portToListen = portFromArgs;
+                }
+            }
+            
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls($"http://0.0.0.0:{_portToListen}/");
     }
 }
